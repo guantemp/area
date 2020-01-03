06399a238bdee95d7bfe0c1e4e6829c85d5a65bc
guantemp <guantemp@126.com>
2020-01-03 14:09:03 +0800

    202001031408

--- core/src/main/java/area/hoprxi/core/domain/model/Province.java
+++ core/src/main/java/area/hoprxi/core/domain/model/Province.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019. www.foxtail.cc rights Reserved.
+ * Copyright (c) 2019. www.hoprxi.com rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
