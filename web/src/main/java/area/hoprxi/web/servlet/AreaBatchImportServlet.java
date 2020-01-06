d56c28a9f2b99c4e21640725dc1a69737026f8b7
guantemp <guantemp@126.com>
2020-01-06 22:50:54 +0800

    202001062250

--- web/src/main/java/area/hoprxi/web/servlet/AreaBatchImportServlet.java
+++ web/src/main/java/area/hoprxi/web/servlet/AreaBatchImportServlet.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2019. www.hoprxi.com rights Reserved.
+ * Copyright (c) 2020. www.hoprxi.com All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ *  Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package area.hoprxi.web.servlet;
@@ -29,7 +29,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /***
@@ -42,42 +43,66 @@ public class AreaBatchImportServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter writer = response.getWriter();
-        writer.close();
+        response.getWriter().println("我看我变没有");
+        response.getWriter().close();
+        System.out.println("test one");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-        if (isMultipart) {
-            // Create a factory for disk-based file items
-            DiskFileItemFactory factory = new DiskFileItemFactory();
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
+        // Create a factory for disk-based file items
+        DiskFileItemFactory factory = new DiskFileItemFactory();
 
-            // Configure a repository (to ensure a secure temp location is used)
-            ServletContext servletContext = this.getServletConfig().getServletContext();
-            File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
-            factory.setRepository(repository);
+        // Configure a repository (to ensure a secure temp location is used)
+        ServletContext servletContext = getServletContext();
+        System.out.println(servletContext.getRealPath("tempdir"));
+        factory.setRepository(new File(servletContext.getRealPath("tempdir")));
+        factory.setSizeThreshold(1024 * 1024);
 
-            // Create a new file upload handler
-            ServletFileUpload upload = new ServletFileUpload(factory);
-            // Set overall request size constraint
-            //upload.setSizeMax(yourMaxRequestSize);
-
-            // Parse the reques
-            try {
-                List<FileItem> items = upload.parseRequest(request);
-                for (FileItem item : items) {
-                    if (!item.isFormField()) {
-                        ServletContext sctx = getServletContext();
-                        File uploadedFile = new File(sctx.getContextPath() + File.separator + "test.xsl");
-                        item.write(uploadedFile);
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        // Set overall request size constraint
+        upload.setSizeMax(100 * 1024 * 1024);
+        // Parse the reques
+        try {
+            List<FileItem> items = upload.parseRequest(request);
+            for (FileItem item : items) {
+                if (item.isFormField()) {//判断是否是文件流
+                    String va = item.getString("UTF-8");
+                    //	System.out.println(name+"="+va);
+                    ///		request.setAttribute(name, value);
+                } else {
+                    System.out.println(items);
+                    File uploadedFile = new File(servletContext.getRealPath("/upload") + File.separator + dateFolderName() + File.separator + "test.xls");
+                    File fileParent = uploadedFile.getParentFile();
+                    if (!fileParent.exists()) {
+                        fileParent.mkdirs();// 创建多级目录
                     }
+                    if (!uploadedFile.exists())
+                        uploadedFile.createNewFile();
+                    item.write(uploadedFile);
                 }
-            } catch (FileUploadException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    private String dateFolderName() {
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doOptions(req, resp);
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+        resp.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1");
+        resp.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        resp.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
     }
 }
